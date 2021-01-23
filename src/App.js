0b863f41a11ce0cd8a9559b51743b991bfa0d762
Mihreteab <setegnmihreteab@gmail,com>
2021-01-23 03:17:08 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Mihreteab Goffa Gozda
+         Goffa Gara
         </a>
       </header>
     </div>
