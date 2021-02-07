f8aa20eca998ecc341d031e38aa7156d5eaff607
Mihreteab <setegnmihreteab@gmail,com>
2021-02-07 12:49:46 -0800

    Great changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-// import logo from './logo.svg';
-import yodit from './yodit.jpg'
+import React from 'react';
 import './App.css';
-// import Button from '@material-ui/core/Button'
-import DenseAppBar from './DenseAppBar'
+import Footer from './Footer';
+import Header from './Header';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+// import Container from '@material-ui/core/Container';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+import Main from './Main';
+// import Footer from './Footer';
+import MainFeaturedPost from './MainFeaturedPost';
+import FeaturedPost from './FeaturedPost';
+// import Main from './Main';
+import Sidebar from './Sidebar';
+
+//posts
+import post1 from './blog-post.1.md';
+import post2 from './blog-post.2.md';
+import post3 from './blog-post.3.md';
+
+const mainFeaturedPost = {
+  title: ' Goffa Gara Organization Official Website',
+  description:
+    " We are Group of individuals mainly inspired to help our community and give back in many different ways. Being originally from Goffa area, and located in most part of the world,we are dedicated in providing multiple assists to those in need.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Learn more about Goffa Area...',
+};
+
+const posts = [post1, post2, post3];
+
+const featuredPosts = [
+  {
+    title: 'Visit Goffa',
+    date: 'Feb 7, 2021',
+    description:
+      'Goffa is a wonder land of multiple scenary.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Goffa Area',
+
+  },
+  {
+    title: 'Support Us',
+    date: 'Feb 7, 2021',
+    description:
+      'There are multiple ways you can support us.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+];
+
+const sidebar = {
+  title: 'Goffa Gara Blogs',
+  description:
+    'The following are list of posts we made since April 1999 on Goffa Gara',
+  archives: [
+    { title: 'March 2020', url: '#' },
+    { title: 'February 2020', url: '#' },
+    { title: 'January 2020', url: '#' },
+    { title: 'November 1999', url: '#' },
+    { title: 'October 1999', url: '#' },
+    { title: 'September 1999', url: '#' },
+    { title: 'August 1999', url: '#' },
+    { title: 'July 1999', url: '#' },
+    { title: 'June 1999', url: '#' },
+    { title: 'May 1999', url: '#' },
+    { title: 'April 1999', url: '#' },
+  ],
+  social: [
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'Instagram', icon: InstagramIcon },
+    { name: 'Facebook', icon: FacebookIcon },
+  ],
+};
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 function App() {
-  return ( 
-    <div className="App">
-      <DenseAppBar/>
-      {/* <header className="App-header"> */}
-        {/* <Button variant="contained" color="teritiary" align="left">
-          Hello World
-        </Button> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={yodit} alt="logo" />
-
-
-      {/* </header> */}
-    </div>
+  
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      {/* <Container maxWidth="lg"> */}
+        <div className="App">
+          <Header/>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+                {featuredPosts.map((post) => (
+                  <FeaturedPost key={post.title} post={post} />
+                ))}
+          </Grid>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title="Today's Post" posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+          <Footer/>
+        </div>
+      {/* </Container> */}
+    </React.Fragment>
   );
 }
-
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,192 @@
+import { AppBar, Toolbar, Typography, makeStyles, Button, IconButton, Drawer, MenuItem, Link  } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import gofagara from "./gofagara.png"
+
+
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: "#2E3B55",
+     paddingRight: "118px",
+     "@media (max-width: 900px)": {
+      paddingLeft: 0,
+    },
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    // textAlign: "left",
+    paddingTop: 20,
+    
+  },
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+    paddingLeft: "0px",
+ },
+ toolbar: {
+  display: "flex",
+  justifyContent: "space-between",
+},
+imgStyle: {
+  maxWidth: 60,
+  // justifyContent: "space-between",
+  justify: "center",
+  paddingTop: "10px",
+  paddingRight: "15px",
+  alignItems: "left"
+ 
+},
+}));
+
+const headersData = [
+  {
+    label: "About",
+    href: "/About",
+  },
+  {
+    label: "Support Us",
+    href: "/Support",
+  },
+  {
+    label: "My Account",
+    href: "/account",
+  },
+  {
+    label: "Log Out",
+    href: "/logout",
+  },
+];
+
+
+export default function Header() {
+
+  useEffect(() => {
+    document.title = "Gofagara.org"
+ }, []);
+
+  const { header, logo, menuButton, toolbar, imgStyle } = useStyles();
+
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false
+  })
+const { mobileView, drawerOpen  } = state;
+
+  const displayMobile = () => {
+
+    const handleDrawerOpen = () =>
+    setState((prevState) => ({ ...prevState, drawerOpen: true }));
+  const handleDrawerClose = () =>
+    setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+    return (
+      <Toolbar>
+        
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+          {/* {<img src={gofagara} alt="gofa gara!" className={imgStyle} />} */}
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div>{getDrawerChoices()}</div>
+        </Drawer>
+  <div>{femmecubatorLogo}</div>
+  </Toolbar>
+    );
+  };
+
+  const getDrawerChoices = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Link
+          {...{
+            component: RouterLink,
+            to: href,
+            color: "inherit",
+            style: { textDecoration: "none" },
+            key: label,
+          }}
+        >
+          <MenuItem>{label}</MenuItem>
+        </Link>
+      );
+    });
+  };
+
+
+
+  
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+
+  const displayDesktop = () => {
+    return <Toolbar className={toolbar}>
+      {femmecubatorLogo}
+      {getMenuButtons()}
+      </Toolbar>;
+  };
+
+
+  const femmecubatorLogo = ( <div flexWrap="wrap"  flexDirection= "row">
+      <Typography display="inline"> <img src={gofagara} alt="gofa gara!" className={imgStyle} /></Typography> 
+      {/* <Typography  variant="h6" component="h1" className={logo} display="inline">
+        Goffa Gara
+      </Typography> */}
+      </div>
+  );
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: RouterLink,
+            className: menuButton
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
+  
+  return (
+    <header>
+      <AppBar  className={header}>
+         {mobileView ? displayMobile() : displayDesktop()}
+         </AppBar>
+    </header>
+  );
+
+  
+}
