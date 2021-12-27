54a939a8884ae87ae953956093f5bf19b732bde1
Mihreteab Setegn <mihreteabsetegn@Mihreteabs-MacBook-Pro.local>
2021-12-26 20:09:31 -0800

    minor 6

--- src/components/supportMethods/index.js
+++ src/components/supportMethods/index.js
@@ -13,13 +13,14 @@ const SupportMethods = () => {
                 marginLeft: "10px",
                 marginRight: "10px",
                 }}>
-                Want to help us <span style={{fontSize: "200px"}}>help</span> those in need? 
+                Want to help us <span style={{fontSize: "30px"}}>help</span> those in need? 
                 please find below ways to give back to the community.
             </h2>
             <div>
                 <span>
                     <DoubleArrowIcon style={{
-                        color: "white"
+                        color: "white",
+                        marginBottom: "10px"
                     }}/>
                     <span style={{
                         color: "white",
@@ -29,14 +30,15 @@ const SupportMethods = () => {
                         Donate Via GoFundMe Account set and administered by the association
                     </span>
                     <Link href="https://www.gofundme.com/f/gara-members-monthly-contribution?utm_campaign=p_cf+share-flow-1&utm_medium=copy_link&utm_source=customer" target="_blank"
-                    style={{color: "#FA8072", paddingLeft: "5px", fontSize: "50px", fontFamily: "fantasy"}}
+                    style={{color: "#FA8072", paddingLeft: "5px", fontSize: "30px", fontFamily: "fantasy"}}
                     >GoFundMe</Link>
                 </span>
             </div>
             <div>
                 <span>
                     <DoubleArrowIcon style={{
-                        color: "white"
+                        color: "white", 
+                        marginBottom: "10px" 
                     }}/>
                     <span style={{
                         color: "white",
@@ -60,12 +62,12 @@ const SupportMethods = () => {
                         paddingLeft: "5px",
                         fontFamily: "serif"
                     }}>
-                        Donate Via <span style={{color: "#FA8072", fontSize: "50px", fontFamily: "fantasy"}}> Check </span>, Account set and administered by the association.
+                        Donate Via <span style={{color: "#FA8072", fontSize: "30px", fontFamily: "fantasy"}}> Check </span>, Account set and administered by the association.
                     </span>
                     <p style={{color: "white"}}>Note: please write "Pay to "GARA" or "Greater Gofa 
                         Relief Association" ".
                     </p>
-                    <span style={{color: "white", fontFamily: "fantasy"}}>
+                    <span style={{color: "white"}}>
                         Mailing information = [
                             Name = Samuel Dossa Morgamo, 
                             Address = 1943 Lexington Avenue, Apt. 12, 
